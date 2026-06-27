911a53c44d37ee1eb6cf3c9ed550c65fd3f1c7e0
PriyanshuVatsal08 <priyanshuvatsal2572004@gmail.com>
2026-06-27 18:01:07 +0530

    Event based project

---
+++ Form to Card/script.js
@@ -0,0 +1,18 @@
+let submitt=document.querySelector("#submitt");
+
+let inputs=document.querySelectorAll("input");
+
+let p=document.querySelectorAll("p");
+
+let profile=document.querySelector("img");
+
+submitt.addEventListener("click",function(e){
+    e.preventDefault();
+    profile.setAttribute("src",`${inputs[0].value}`);
+    p[0].textContent=inputs[1].value;
+    p[1].textContent=inputs[2].value;
+    p[2].textContent=inputs[3].value;
+});                                     
+
+
+
