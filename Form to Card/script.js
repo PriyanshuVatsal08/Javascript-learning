e77f481324a470f584cee299f1702840d6f1fb14
PriyanshuVatsal08 <priyanshuvatsal2572004@gmail.com>
2026-06-28 11:25:35 +0530

    Add css to form to card project

--- Form to Card/script.js
+++ Form to Card/script.js
@@ -1,18 +1,23 @@
-let submitt=document.querySelector("#submitt");
+let submitt = document.querySelector("#submitt");
 
-let inputs=document.querySelectorAll("input");
+let inputs = document.querySelectorAll("input");
 
-let p=document.querySelectorAll("p");
+let p = document.querySelectorAll("p");
 
-let profile=document.querySelector("img");
+let profile = document.querySelector("img");
 
-submitt.addEventListener("click",function(e){
+submitt.addEventListener("click", function (e) {
     e.preventDefault();
-    profile.setAttribute("src",`${inputs[0].value}`);
-    p[0].textContent=inputs[1].value;
-    p[1].textContent=inputs[2].value;
-    p[2].textContent=inputs[3].value;
-});                                     
+    profile.setAttribute("src", `${inputs[0].value}`);
+    p[0].textContent = inputs[1].value;
+    p[1].textContent = inputs[2].value;
+    p[2].textContent = inputs[3].value;
+});
+
+
+document.querySelector("ul").addEventListener("click", function (e) {
+    e.target.classList.toggle("lt");
+});
 
 
 
